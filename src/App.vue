9a53139df186223f54795c24b611cc376babe42a
Charles Roh <chuckyroh@gmail.com>
2020-10-15 00:42:50 -0700

    Initial commit, add p5 and lint

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+	<CanvasOne />
+</template>
+
+<script>
+import CanvasOne from './components/CanvasOne.vue'
+
+export default {
+	name: 'App',
+	components: {
+		CanvasOne
+	}
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/CanvasOne.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="canvas-one">
+		Testing
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'CanvasOne',
+	props: {
+
+	}
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
